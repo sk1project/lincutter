45fcc43f5565d81a8d958bcb4b72887e707089a6
Igor.E.Novikov <Igor.E.Novikov@gmail.com>
2013-05-26 12:35:10 +0000

    Package description

--- setup.py
+++ setup.py
@@ -43,3 +43,46 @@ from libutils import make_source_list, DEB_Builder
 #Flags
 DEB_PACKAGE = False
 CLEAR_BUILD = False
+
+############################################################
+#
+# Package description
+#
+############################################################
+NAME = 'lincutter'
+VERSION = '1.0'
+DESCRIPTION = 'Vector graphics editor for cutting plotters'
+AUTHOR = 'Igor E. Novikov'
+AUTHOR_EMAIL = 'igor.e.novikov@gmail.com'
+MAINTAINER = AUTHOR
+MAINTAINER_EMAIL = AUTHOR_EMAIL
+LICENSE = 'GPL v3'
+URL = 'http://sk1project.org'
+DOWNLOAD_URL = 'http://sk1project.org'
+CLASSIFIERS = [
+'Development Status :: 5 - Stable',
+'Environment :: Desktop',
+'Intended Audience :: End Users/Desktop',
+'License :: OSI Approved :: GPL v3',
+'Operating System :: POSIX',
+'Operating System :: MacOS :: MacOS X',
+'Programming Language :: Python',
+"Topic :: Multimedia :: Graphics :: Editors :: Vector-Based",
+]
+LONG_DESCRIPTION = '''
+LinCutter is an open source vector graphics editor for cutting plotters similar 
+to Roland CutStudio, Artcut, or PostCut. The software is designed for HPGL 
+compatible cutting plotters. 
+
+sK1 Project (http://sk1project.org),
+Copyright (C) 2011-2013 by Igor E. Novikov
+'''
+LONG_DEB_DESCRIPTION = ''' .
+ LinCutter is an open source vector graphics editor for cutting plotters similar 
+ to Roland CutStudio, Artcut, or PostCut. The software is designed for HPGL 
+ compatible cutting plotters.
+ . 
+ sK1 Project (http://sk1project.org),
+ Copyright (C) 2011-2013 by Igor E. Novikov 
+ .
+'''
